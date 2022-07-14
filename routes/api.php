91a8093653b451c19a90f7dbc7179afbcee0c073
Hejo <59996938+DevHejo@users.noreply.github.com>
2022-07-14 20:36:14 +0530

    Oauth2 added
    Oauth2 is added using laravel passport. Most of the steps to make a working authenticated route is done

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// controllers
+use App\Http\Controllers\AuthorsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:api')->prefix('v1')->group(function() {
+    Route::get('/user', function(Request $request) {
+        return $request->user();
+    });
+    Route::get('/authors/{author}', [AuthorsController::class, 'show']);
+});
+
+
+Route::get('/test', function(Request $request){
+    $test = 'Test Route';
+    return response()->json($test);
 });
